27123445f89d373cf0227b45ec6d472081b2b114
Alejandro Sosa <alejandrososadev@gmail.com>
2024-12-25 15:49:35 -0400

    Adds Product and User services Adds JwtSettings class

---
+++ WBAPI/Configuration/JwtSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace WBAPI.Configuration;
+
+public class JwtSettings
+{
+    public string Key { get; set; }
+
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public JwtSettings()
+    {
+        Key = String.Empty;
+        Issuer = String.Empty;
+        Audience = String.Empty;
+    }
+
+    public JwtSettings(string key, string issuer, string audience)
+    {
+        Key = key;
+        Issuer = issuer;
+        Audience = audience;
+    }
+}
+
